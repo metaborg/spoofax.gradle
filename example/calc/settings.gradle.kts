a1a2002052d87560a7b70bdcc19e37aa74719815
Gabriel Konat <gabrielkonat@gmail.com>
2019-03-13 15:06:16 +0100

    Make Spoofax language specification build more incremental by approximating dependencies.

--- example/calc/settings.gradle.kts
+++ example/calc/settings.gradle.kts
@@ -2,8 +2,16 @@ rootProject.name = "org.metaborg.lang.calc"
 
 pluginManagement {
   repositories {
+    // Maven local for locally published builds of spoofax.gradle, until IntelliJ properly supports composite builds with plugins.
+    mavenLocal()
+
     maven(url = "https://artifacts.metaborg.org/content/repositories/releases/")
     maven(url = "https://artifacts.metaborg.org/content/repositories/snapshots/")
+    // Required by `org.metaborg.spoofax.gradle.langspec` plugin.
+    maven("https://pluto-build.github.io/mvnrepository/")
+    maven("https://sugar-lang.github.io/mvnrepository/")
+    maven("http://nexus.usethesource.io/content/repositories/public/")
+
     gradlePluginPortal()
   }
 }
