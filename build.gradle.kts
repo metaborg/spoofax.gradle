2ab994c13f5cdfda7a7abec1707cacdc0e49c659
Gabriel Konat <gabrielkonat@gmail.com>
2019-03-19 13:20:16 +0100

    Update plugin versions.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 plugins {
-  id("org.metaborg.gradle.config.root-project") version "0.3.5"
-  id("org.metaborg.gradle.config.kotlin-gradle-plugin") version "0.3.5"
-  id("org.metaborg.gradle.config.junit-testing") version "0.3.5"
-  id("org.metaborg.gitonium") version "0.1.0"
-  kotlin("jvm") version "1.3.20"
+  id("org.metaborg.gradle.config.root-project") version "0.3.6"
+  id("org.metaborg.gradle.config.kotlin-gradle-plugin") version "0.3.6"
+  id("org.metaborg.gradle.config.junit-testing") version "0.3.6"
+  id("org.metaborg.gitonium") version "0.1.1"
+  kotlin("jvm") version "1.3.21"
   `kotlin-dsl`
 }
 
--- example/calc/build.gradle.kts
+++ example/calc/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-  id("org.metaborg.gradle.config.root-project") version "0.3.3"
-  id("org.metaborg.gitonium") version "0.1.0"
+  id("org.metaborg.gradle.config.root-project") version "0.3.6"
+  id("org.metaborg.gitonium") version "0.1.1"
   id("org.metaborg.spoofax.gradle.langspec") // No version, use included build only.
   id("de.set.ecj") version "1.4.1" // Use ECJ to speed up compilation of Stratego's generated Java files.
 }
