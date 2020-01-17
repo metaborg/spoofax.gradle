e826b0103b9bcc1dd306a7b78c3f001a0444b9b1
Gabriel Konat <gabrielkonat@gmail.com>
2020-01-17 11:46:49 +0100

    Do not embed artifacts into plugin, it doesn't work properly for some reason.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
   id("org.metaborg.gradle.config.root-project") version "0.3.12"
   id("org.metaborg.gradle.config.kotlin-gradle-plugin") version "0.3.12"
   id("org.metaborg.gitonium") version "0.1.2"
-  kotlin("jvm") version "1.3.61"
+  kotlin("jvm") version "1.3.41" // Use 1.3.41 to keep in sync with embedded Kotlin version of Gradle 5.6.4.
   `kotlin-dsl`
 }
 
@@ -11,9 +11,17 @@ metaborg {
   kotlinLanguageVersion = "1.2"
 }
 
-gitonium {
-  // Disable snapshot dependency checks for releases, until we depend on a stable version of Spoofax Core.
-  checkSnapshotDependenciesInRelease = false
+// Repositories required for transitive dependencies of 'spoofax.meta.core'.
+repositories {
+  maven("https://pluto-build.github.io/mvnrepository/")
+  maven("https://sugar-lang.github.io/mvnrepository/")
+  maven("http://nexus.usethesource.io/content/repositories/public/")
+}
+
+val spoofaxVersion = "2.6.0-SNAPSHOT"
+dependencies {
+  api("org.metaborg:org.metaborg.spoofax.meta.core:$spoofaxVersion")
+  api("org.metaborg:org.metaborg.spt.core:$spoofaxVersion")
 }
 
 gradlePlugin {
@@ -29,29 +37,7 @@ gradlePlugin {
   }
 }
 
-// Embed Spoofax Core dependencies into the plugin so that users do not receive the transitive dependency tree.
-val embedded: Configuration = configurations.create("embedded")
-configurations.getByName(JavaPlugin.COMPILE_ONLY_CONFIGURATION_NAME).extendsFrom(embedded)
-val spoofaxVersion = "2.6.0-SNAPSHOT"
-dependencies {
-  embedded("org.metaborg:org.metaborg.spoofax.meta.core:$spoofaxVersion")
-  embedded("org.metaborg:org.metaborg.spt.core:$spoofaxVersion")
-}
-tasks {
-  jar {
-    // Closure inside from to defer evaluation of configuration until task execution time.
-    from({ embedded.filter { it.exists() }.map { if(it.isDirectory) it else zipTree(it) } }) {
-      // Exclude signature files from dependencies, otherwise the JVM will refuse to load the created JAR file.
-      exclude("META-INF/*.SF", "META-INF/*.DSA", "META-INF/*.RSA")
-    }
-    // Enable zip64 to support ZIP files with more than 2^16 entries, which we need.
-    isZip64 = true
-  }
-}
-
-// Repositories required for transitive dependencies of 'spoofax.meta.core'.
-repositories {
-  maven("https://pluto-build.github.io/mvnrepository/")
-  maven("https://sugar-lang.github.io/mvnrepository/")
-  maven("http://nexus.usethesource.io/content/repositories/public/")
+gitonium {
+  // Disable snapshot dependency checks for releases, until we depend on a stable version of Spoofax Core.
+  checkSnapshotDependenciesInRelease = false
 }
