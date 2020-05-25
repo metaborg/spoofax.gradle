eed6b222d83711b460a81c3a5aeaa721fdf40164
Gabriel Konat <gabrielkonat@gmail.com>
2020-05-25 13:33:43 +0200

    Fix typo and add comment.

--- example/build.gradle.kts
+++ example/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
   id("org.metaborg.gitonium") version "0.1.2"
 
   // Set versions for plugins to use, only applying them in subprojects (apply false here).
-  id("org.metaborg.spoofax.gradle.langspec") apply false
+  id("org.metaborg.spoofax.gradle.langspec") apply false // No version: use the plugin from the included composite build
   id("org.metaborg.spoofax.gradle.project") apply false
   id("org.metaborg.spoofax.gradle.test") apply false
   id("de.set.ecj") version "1.4.1" apply false
