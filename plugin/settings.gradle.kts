48d5e145f229d76d1d0be5108dda26bf59277862
Daniel A. A. Pelsmaeker <developer@pelsmaeker.net>
2024-07-09 13:50:28 +0200

    Add foojay plugin

--- plugin/settings.gradle.kts
+++ plugin/settings.gradle.kts
@@ -4,4 +4,8 @@ pluginManagement {
   repositories {
     maven("https://artifacts.metaborg.org/content/groups/public/")
   }
+
+// This downloads an appropriate JVM if not already available
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
