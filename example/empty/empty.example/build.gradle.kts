8e2bc687d721b609b7c2de5577e4ba7c3e9771a8
Daniel A. A. Pelsmaeker <developer@pelsmaeker.net>
2024-07-19 10:56:07 +0200

    Add explicit language source/compile dependencies

--- example/empty/empty.example/build.gradle.kts
+++ example/empty/empty.example/build.gradle.kts
@@ -4,16 +4,20 @@ plugins {
     id("org.metaborg.devenv.spoofax.gradle.project")
 }
 
-dependencies {
-    compileLanguage(project(":empty"))
-    
-    compileOnly(libs.spoofax.core)
-}
-
 spoofaxProject {
+    addCompileDependenciesFromMetaborgYaml.set(false)
+    addSourceDependenciesFromMetaborgYaml.set(false)
+
     // Fixes: Could not resolve all dependencies for configuration '<project>:compileClasspath'.
     //  The project declares repositories, effectively ignoring the repositories you have declared in the settings.
     addSpoofaxRepository.set(false)
     // Fixes: Could not find org.metaborg:org.metaborg.spoofax.core:property(java.lang.String)
     addSpoofaxCoreDependency.set(false)
+}
+
+
+dependencies {
+    compileLanguage(project(":empty"))
+
+    compileOnly(libs.spoofax.core)
 }
